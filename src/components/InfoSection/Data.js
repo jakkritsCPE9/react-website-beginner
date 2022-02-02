b9c81269ecee20df529a4f85b3b93eb0672f113c
jakkritsCPE9 <jakkrits59@nu.ac.th>
2022-02-03 00:04:27 +0700

    update add infoSection components.

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: 'about',
+  light: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transection with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to sand unlimited transaction without getting charged any fees.',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: require('../../images/svg-1.svg').default,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false
+};
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-scroll";
+import { Button } from "../ButtonElements";
 import {
   InfoContainer,
   InfoWrapper,
@@ -12,28 +12,40 @@ import {
   Column1,
   ImgWrap,
   Column2,
-  Img
+  Img,
 } from "./InfoElement";
 
-const InfoSection = () => {
+const InfoSection = ({
+  lightBg,
+  id,
+  imgStart,
+  topLine,
+  lightText,
+  headline,
+  darkText,
+  description,
+  buttonLabel,
+  img,
+  alt,
+}) => {
   return (
     <>
-      <InfoContainer>
+      <InfoContainer lightBg={lightBg} id={id}>
         <InfoWrapper>
-          <InfoRow>
+          <InfoRow imgStart={imgStart}>
             <Column1>
               <TextWrapper>
-                <TopLine>TopLine</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subtitle</Subtitle>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
                 <BtnWrap>
-                  <Button to="home" />
+                  <Button to="home">{buttonLabel}</Button>
                 </BtnWrap>
               </TextWrapper>
             </Column1>
             <Column2>
               <ImgWrap>
-                <Img />
+                <Img src={img} alt={alt} />
               </ImgWrap>
             </Column2>
           </InfoRow>
